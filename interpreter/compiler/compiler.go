9e80a8b4fb4b820b3fd85724d5e3ec936dad5acc
ObscureBrandon <thegamingcop2005@gmail.com>
2024-09-13 19:12:32 +0300

    feat: Hashes

--- interpreter/compiler/compiler.go
+++ interpreter/compiler/compiler.go
@@ -5,6 +5,7 @@ import (
 	"eventloop/interpreter/interpreter/code"
 	"eventloop/interpreter/interpreter/object"
 	"fmt"
+	"sort"
 )
 
 type EmittedInstruction struct {
@@ -57,6 +58,30 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(code.OpArray, len(node.Elements))
+
+	case *ast.HashLiteral:
+		keys := []ast.Expression{}
+		for k := range node.Pairs {
+			keys = append(keys, k)
+		}
+
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[j].String()
+		})
+
+		for _, k := range keys {
+			err := c.Compile(k)
+			if err != nil {
+				return err
+			}
+
+			err = c.Compile(node.Pairs[k])
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpHash, len(node.Pairs)*2)
 
 	case *ast.IfExpression:
 		err := c.Compile(node.Condition)
